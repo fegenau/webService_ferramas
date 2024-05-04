b3e1dc5fe51be9fb6291dda62a1d911f4c4ddbc5
Francisco Egenau Navia <fr.egenau@duocuc.cl>
2024-05-03 21:56:14 -0400

    navegacion entre paginas

--- WebCommerce/urls.py
+++ WebCommerce/urls.py
@@ -4,5 +4,14 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('about/', views.about),
+    path('anclajes/', views.anclajes),
+    path('crear-cuenta/', views.crearCuenta),
+    path('equipo-seguridad/', views.equipoSeguridad),
+    path('fijaciones/', views.fijacion),
+    path('herramientas/', views.herramientas),
+    path('inicio-sesion/', views.inicioSesion),
+    path('materiales-basicos',views.materialesBasicos),
+    path('pago/', views.pago),
+    path('suscripcion/', views.suscripcion),
     
 ]
--- WebCommerce/views.py
+++ WebCommerce/views.py
@@ -8,3 +8,29 @@ def index(request):
 def about(request):
     return render(request,'about.html')
 
+def anclajes (request):
+    return render(request,'Anclajes/anclajes-tornillos.html')
+
+def crearCuenta(request):
+    return render(request,'CrearCuenta/crearCuenta.html')
+
+def equipoSeguridad(request):
+    return render(request,'EquiposSeguridad/equipoSeguridad.html')
+
+def fijacion(request):
+    return render(request,'Fijaciones/fijaciones.html')
+
+def herramientas(request):
+    return render(request,'Herramientas/manuales.html')
+
+def inicioSesion (request):
+    return render(request,'InicioSesion/inicioSesion.html')
+
+def materialesBasicos(request):
+    return render(request,'MaterialesBasicos/materialesBasicos.html')
+
+def pago(request):
+    return render(request,'Pago/pago.html')
+
+def suscripcion(request):
+    return render(request,'Suscripcion/suscripcion.html')
