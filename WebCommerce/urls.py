cc0df8bbfda463d9e9c70bc4260fa6feb60752ee
mlaborie <ma.laborie@duocuc.cl>
2024-05-27 15:03:53 -0400

    Login Alpha

--- WebCommerce/urls.py
+++ WebCommerce/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
 
-    path('login/', views.login),
+    path('login/', views.user_login,name='user_login'),
+    
+
     path('register/', views.register),
 
 
--- WebCommerce/views.py
+++ WebCommerce/views.py
@@ -1,13 +1,47 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# Vistas (views.py)
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from .forms import LoginForm
 
+
+def user_login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirige al usuario a una página después del inicio de sesión
+            return redirect('dashboard')  # Cambia 'dashboard' por la URL a la que quieras redirigir
+        else:
+            # El usuario no pudo iniciar sesión, puedes mostrar un mensaje de error
+            return render(request, 'InicioSesion/login.html', {'error_message': 'Invalid email or password'})
+    else:
+        return render(request, 'InicioSesion/login.html')
+
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')  # Redirige a la página de inicio de sesión después del cierre de sesión
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirige a la página de inicio de sesión después del registro exitoso
+    else:
+        form = UserCreationForm()
+    return render(request, 'InicioSesion/register.html', {'form': form})
 
 #Ferramax
 
-def login(request):
-    return render(request,'InicioSesion/login.html')
-def register(request):
-    return render(request,'InicioSesion/register.html')
+#def login(request):
+#    return render(request,'InicioSesion/login.html')
+#def register(request):
+#    return render(request,'InicioSesion/register.html')
 
 
 
